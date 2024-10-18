0657b8b2b4c6910e101e04aa4b9c0005ef74c07c
xinpengg@nycstudents.net <xinpengg@nycstudents.net>
2024-10-18 15:10:03 -0400

    changes

--- index.js
+++ index.js
@@ -4,13 +4,29 @@ function increment () {
     count = count + 1; 
     console.log(count) ;
     document.getElementById("count-el").innerText = count
-
+    validity();
 }
 
 function save () { 
-    
     saves = saves + count + " - ";
     document.getElementById("entries").innerText = "Previous Entries: " + saves ;
 
 }
-// Counter app
+function validity () {
+    if (count > 34 || count < 0) {
+        dismiss()
+        alert("The classroom has reached it's maximum capacity, will reset number back to 0");
+
+    }
+}
+function incrementby10 () {
+    count = count + 10;
+    console.log(count) ;
+    document.getElementById("count-el").innerText = count
+    validity();
+}
+function dismiss () {
+    count = 0;
+    document.getElementById("count-el").innerText = count
+
+}
